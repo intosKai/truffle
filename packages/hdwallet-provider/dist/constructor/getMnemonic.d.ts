1c00c7d147e38036881df58dc972f39f66eb4f6d
intosKai <ivanoff.pg@ya.ru>
2024-03-14 17:27:49 +0300

    add dist

---
+++ packages/hdwallet-provider/dist/constructor/getMnemonic.d.ts
@@ -0,0 +1,5 @@
+import type { SigningAuthority } from "./Constructor";
+import type { Mnemonic } from "./types";
+export declare const getMnemonic: (
+  signingAuthority: SigningAuthority
+) => Mnemonic | undefined;
---
+++ packages/hdwallet-provider/dist/constructor/getOptions.d.ts
@@ -0,0 +1,5 @@
+import type { ConstructorArguments } from "./ConstructorArguments";
+import type * as Constructor from "./Constructor";
+export declare const getOptions: (
+  ...args: ConstructorArguments
+) => Constructor.Options;
